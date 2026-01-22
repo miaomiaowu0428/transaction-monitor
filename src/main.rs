1b77ada8ef66856cbfb39e15c2770327c0903386
Qmm <2268428576@qq.com>
2026-01-22 16:10:21 +0800

    runable

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,10 @@
+use transaction_monitor::tx_monitor_task;
+
 #[tokio::main]
 async fn main(){
+        rustls::crypto::CryptoProvider::install_default(rustls::crypto::ring::default_provider())
+        .unwrap();
+    dotenvy::dotenv().ok();
+    utils::init_logger();
     tx_monitor_task().await;
 }
